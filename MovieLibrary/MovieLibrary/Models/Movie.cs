743683aa855c16ae86705f6106efda92ba02d734
unknown <soccer_Matt_4@yahoo.com>
2019-06-11 13:29:06 -0500

    Added movie class, input seed and movie data and created the database.

---
+++ MovieLibrary/MovieLibrary/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieLibrary.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+        [Display(Name = "Director Name")]
+        public string DirectorName { get; set; }
+        [Display(Name = "Movie Image")]
+        public string MovieImage { get; set; }
+
+
+
+    }
+}
